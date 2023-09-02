77acf578d78aac04ee3d23598281a05c3b6fc5fc
Bara sofiane badis <barasofianebadis@gmail.com>
2023-09-02 22:43:19 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,156 @@
+import '../styles/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-function Footer(){
 
-    return <div>footer</div> 
+
+function Footer() {
+  return  <>
+      {/* Footer Top */}
+      <footer className='py-3'>
+      <div className='container-xxl'>
+        <div className='row align-items-center '>
+          <div className='col-5 d-flex  justify-content-center gap-3'>
+            <FontAwesomeIcon icon={faRocket} size="xl" />
+            <h5>Sign Up for Newsletter</h5>
+          </div>
+          <div className='col-7 '>
+            <div className='input-group'>
+              <input
+                type='email'
+                className='form-control'
+                placeholder='Enter your email'
+                aria-label='Enter your email'
+                aria-describedby='subscribe-button'
+              />
+              <button
+                className='btn btn-primary'
+                type='button'
+                id='subscribe-button'
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+      {/* Footer Center */}
+      <footer className='py-4'>
+      <div className='container-fluid'>
+        <div className='row '>
+          {/* Row 1 */}
+          <div className='col-md-3 text-center'>
+            <div className='mb-4'>
+              <h5>Section 1</h5>
+              <ul className='list-unstyled'>
+                <li>
+                  <Link to='/link1'>Link 1</Link>
+                </li>
+                <li>
+                  <Link to='/link2'>Link 2</Link>
+                </li>
+                <li>
+                  <Link to='/link3'>Link 3</Link>
+                </li>
+                <li>
+                  <Link to='/link4'>Link 4</Link>
+                </li>
+                <li>
+                  <Link to='/link5'>Link 5</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Row 2 */}
+          <div className='col-md-3 text-center'>
+            <div className='mb-4'>
+              <h5>Section 2</h5>
+              <ul className='list-unstyled'>
+                <li>
+                  <Link to='/link6'>Link 6</Link>
+                </li>
+                <li>
+                  <Link to='/link7'>Link 7</Link>
+                </li>
+                <li>
+                  <Link to='/link8'>Link 8</Link>
+                </li>
+                <li>
+                  <Link to='/link9'>Link 9</Link>
+                </li>
+                <li>
+                  <Link to='/link10'>Link 10</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Row 3 */}
+          <div className='col-md-3 text-center'>
+            <div className='mb-4'>
+              <h5>Section 3</h5>
+              <ul className='list-unstyled'>
+                <li>
+                  <Link to='/link11'>Link 11</Link>
+                </li>
+                <li>
+                  <Link to='/link12'>Link 12</Link>
+                </li>
+                <li>
+                  <Link to='/link13'>Link 13</Link>
+                </li>
+                <li>
+                  <Link to='/link14'>Link 14</Link>
+                </li>
+                <li>
+                  <Link to='/link15'>Link 15</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Row 4 */}
+          <div className='col-md-3 text-center'>
+            <div className='mb-4'>
+              <h5>Section 4</h5>
+              <ul className='list-unstyled'>
+                <li>
+                  <Link to='/link16'>Link 16</Link>
+                </li>
+                <li>
+                  <Link to='/link17'>Link 17</Link>
+                </li>
+                <li>
+                  <Link to='/link18'>Link 18</Link>
+                </li>
+                <li>
+                  <Link to='/link19'>Link 19</Link>
+                </li>
+                <li>
+                  <Link to='/link20'>Link 20</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+     
+      {/* Foooter Bottom */}
+      <footer className='py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <p className='text-center mb-0'>&copy; {new Date().getFullYear()} Powered By Barasofianebadis@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
 }
 
-export default Footer ;
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../index.css';
-import { faSearch, faShoppingCart, faHeart, faUser,faPhone ,faSignOut} from '@fortawesome/free-solid-svg-icons';
-
-
+import { faSearch, faShoppingCart, faHeart, faUser, faPhone, faSignOut } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
   return (
@@ -19,167 +17,162 @@ const Header = () => {
             </p>
           </div>
           <div className="col-6 bgd-color d-flex align-items-center justify-content-center pt-1.5">
-            <p className=" custom-very-small bgd-color m-0 text-capitalize">
-            contact :<a href="tel:+221658665062" className=''> +221 658665062 / 0658665062</a> 
-            <a><FontAwesomeIcon icon={faPhone} className='px-2  bgd-color text-black'/> </a>
-            <a href="https://www.facebook.com" className='text-white mx-2 '>
+            <p className="custom-very-small bgd-color m-0 text-capitalize text-black">
+              contact :
+              <a href="tel:+221658665062" className=''> +221 658665062 / 0658665062</a>
+              <a><FontAwesomeIcon icon={faPhone} className='px-2 bgd-color text-black' /> </a>
+              <a href="https://www.facebook.com" className='text-white mx-2 '>
                 <FontAwesomeIcon icon={faFacebookF} className="facebook-icons " />
               </a>
               <a href="https://www.twitter.com" className='text-white mx-2 '>
-                <FontAwesomeIcon icon={faTwitter} className='twitter-icons '/>
-              </a>         
+                <FontAwesomeIcon icon={faTwitter} className='twitter-icons ' />
+              </a>
               <a href="https://www.twitter.com" className='text-white mx-2 '>
-                <FontAwesomeIcon icon={faTelegram} className='twitter-icons '/>
-              </a>  
+                <FontAwesomeIcon icon={faTelegram} className='twitter-icons ' />
+              </a>
             </p>
           </div>
-          
         </div>
       </div>
 
       <div className="main-navbar shadow-sm sticky-top">
-      <div className="top-navbar">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-2 my-auto d-none d-sm-none d-md-block d-lg-block">
-              <h5 className="brand-name">InformatiqueDZ</h5>
+        <div className="top-navbar">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-2 my-auto d-none d-sm-none d-md-block d-lg-block">
+                <h5 className="brand-name">InformatiqueDZ</h5>
+              </div>
+              <div className="col-md-5 my-auto">
+                <form role="search">
+                  <div className="input-group">
+                    <input type="search" placeholder="Search your product" className="form-control" />
+                    <button className="btn bg-white" type="submit">
+                      <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-md-5 my-auto">
+                <ul className="nav justify-content-end">
+                  <li className="nav-item">
+                    <Link to="/cart" className="nav-link">
+                      <FontAwesomeIcon icon={faShoppingCart} /> Cart (0)
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/wishlist" className="nav-link">
+                      <FontAwesomeIcon icon={faHeart} /> Wishlist (0)
+                    </Link>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <Link
+                      to="/user/profile"
+                      className="nav-link dropdown-toggle"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <FontAwesomeIcon icon={faUser} /> Username
+                    </Link>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li>
+                        <Link to="/user/profile" className="dropdown-item">
+                          <FontAwesomeIcon icon={faUser} /> Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/user/wishlist" className="dropdown-item">
+                          <FontAwesomeIcon icon={faHeart} /> My Wishlist
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/user/cart" className="dropdown-item">
+                          <FontAwesomeIcon icon={faShoppingCart} /> My Cart
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/logout" className="dropdown-item">
+                          <FontAwesomeIcon icon={faSignOut} /> Logout
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-md-5 my-auto">
-              <form role="search">
-                <div className="input-group">
-                  <input type="search" placeholder="Search your product" className="form-control" />
-                  <button className="btn bg-white" type="submit">
-                    <FontAwesomeIcon icon={faSearch} />
-                  </button>
-                </div>
-              </form>
-            </div>
-            <div className="col-md-5 my-auto">
-              <ul className="nav justify-content-end">
+          </div>
+        </div>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand d-block d-sm-block d-md-none d-lg-none">
+              InformatiqueDZ
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <FontAwesomeIcon icon={faShoppingCart} /> Cart (0)
-                  </a>
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <FontAwesomeIcon icon={faHeart} /> Wishlist (0)
-                  </a>
+                  <Link to="/categories" className="nav-link">
+                    All Categories
+                  </Link>
                 </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <FontAwesomeIcon icon={faUser} /> Username
-                  </a>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        <FontAwesomeIcon icon={faUser} /> Profile
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        <FontAwesomeIcon icon={faHeart} /> My Wishlist
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        <FontAwesomeIcon icon={faShoppingCart} /> My Cart
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        <FontAwesomeIcon icon={faSignOut} /> Logout
-                      </a>
-                    </li>
-                  </ul>
+                <li className="nav-item">
+                  <Link to="/new-arrivals" className="nav-link">
+                    New Arrivals
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/featured-products" className="nav-link">
+                    Featured Products
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/electronics" className="nav-link">
+                    Electronics
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/fashions" className="nav-link">
+                    Fashions
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/accessories" className="nav-link">
+                    Accessories
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/home" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/appliances" className="nav-link">
+                    Appliances
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-        </div>
+        </nav>
       </div>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <a className="navbar-brand d-block d-sm-block d-md-none d-lg-none" href="#">
-            InformatiqueDZ
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  All Categories
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  New Arrivals
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Featured Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Electronics
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Fashions
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Accessories
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Appliances
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  
     </header>
-
-  
-  
-
-  )  
+  );
 };
 
 export default Header;
